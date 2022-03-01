398520214fc7831339fd9611268e11a56398b598
unknown <sakibkhan207@gmail.com>
2022-03-01 16:45:44 +0600

    readme updatedd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Shopify App
+                    Laravel Shopify App Working
                 </div>
             </div>
         </div>
